8cf8850c6c525780fe7685a844318cca60025ff5
Santiago Ramos Morales <santiagoramos198@gmail.com>
2022-03-24 11:06:09 -0600

    Listo, probado con el módulo unittest

--- test_validate_pin.py
+++ test_validate_pin.py
@@ -1,35 +1,33 @@
-import test
+import unittest
 from validate_pin import validate_pin
 
-def run_tests():
-    @test.it("should return False for pins with length other than 4 or 6")
-    def basic_test_cases():
-        test.assert_equals(validate_pin("1"),False, "Wrong output for '1'")
-        test.assert_equals(validate_pin("12"),False,"Wrong output for '12'")
-        test.assert_equals(validate_pin("123"),False,"Wrong output for '123'")
-        test.assert_equals(validate_pin("12345"),False,"Wrong output for '12345'")
-        test.assert_equals(validate_pin("1234567"),False,"Wrong output for '1234567'")
-        test.assert_equals(validate_pin("-1234"),False,"Wrong output for '-1234'")
-        test.assert_equals(validate_pin("-12345"),False,"Wrong output for '-12345'")
-        test.assert_equals(validate_pin("1.234"),False,"Wrong output for '1.234'")
+
+class TestValidatePin(unittest.TestCase):
+    def test_validate_pin(self):
+        self.assertEqual(validate_pin("1"),False, "Wrong output for '1'")
+        self.assertEqual(validate_pin("12"),False,"Wrong output for '12'")
+        self.assertEqual(validate_pin("123"),False,"Wrong output for '123'")
+        self.assertEqual(validate_pin("12345"),False,"Wrong output for '12345'")
+        self.assertEqual(validate_pin("1234567"),False,"Wrong output for '1234567'")
+        self.assertEqual(validate_pin("-1234"),False,"Wrong output for '-1234'")
+        self.assertEqual(validate_pin("-12345"),False,"Wrong output for '-12345'")
+        self.assertEqual(validate_pin("1.234"),False,"Wrong output for '1.234'")
     
 
-    @test.it("should return Falsa for pins wich contain characters other than digits")
-    def _():
-        test.assert_equals(validate_pin("a234"),False, "Wrong output for 'a234'")
-        test.assert_equals(validate_pin(".234"),False, "Wrong output for '.234'")
+    def _(self):
+        self.assertEqual(validate_pin("a234"),False, "Wrong output for 'a234'")
+        self.assertEqual(validate_pin(".234"),False, "Wrong output for '.234'")
 
 
-    @test.it("shold return True for valid pins")
-    def _():
-        test.assert_equals(validate_pin("1234"),True,"Wrong output for '1234'")
-        test.assert_equals(validate_pin("0000"),True,"Wrong output for '0000'")
-        test.assert_equals(validate_pin("1111"),True,"Wrong output for '1111'")
-        test.assert_equals(validate_pin("123456"),True,"Wrong output for '123456'")
-        test.assert_equals(validate_pin("098765"),True,"Wrong output for '098765'")
-        test.assert_equals(validate_pin("000000"),True,"Wrong output for '000000'")
-        test.assert_equals(validate_pin("123456"),True,"Wrong output for '123456'")
-        test.assert_equals(validate_pin("090909"),True,"Wrong output for '090909'")
+    def _(self):
+        self.assertEqual(validate_pin("1234"),True,"Wrong output for '1234'")
+        self.assertEqual(validate_pin("0000"),True,"Wrong output for '0000'")
+        self.assertEqual(validate_pin("1111"),True,"Wrong output for '1111'")
+        self.assertEqual(validate_pin("123456"),True,"Wrong output for '123456'")
+        self.assertEqual(validate_pin("098765"),True,"Wrong output for '098765'")
+        self.assertEqual(validate_pin("000000"),True,"Wrong output for '000000'")
+        self.assertEqual(validate_pin("123456"),True,"Wrong output for '123456'")
+        self.assertEqual(validate_pin("090909"),True,"Wrong output for '090909'")
 
 if __name__ == "__main__":
-    run_tests()
+    unittest.main()
